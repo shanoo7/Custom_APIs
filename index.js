2de22df7776dd9a79cd40c2236f2be3f20f93eb3
shanoo7 <shanoo.kumar11@gmail.com>
2024-03-03 15:50:48 +0530

    changing some info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
-import cors from 'cors'
+import dotenv from "dotenv"
 const app = express();
-app.use(cors())
+ dotenv.config()
 
 
 app.get('/api/products', (req, res) => {
@@ -83,7 +83,7 @@ app.get('/api/products', (req, res) => {
 
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log("app is running at port :" + port )
 })
