78e70b8ea5ffaa51170484a5877b8cd5316357aa
shanoo7 <shanoo.kumar11@gmail.com>
2024-03-03 16:54:57 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 const app = express();
  dotenv.config()
+ app.use(cors())
 
 
 app.get('/api/products', (req, res) => {
